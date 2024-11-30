35c64a7f04cdcd347d1d2544179b555b552dfc67
HrishikeshSatkar <hrishikeshsatkar007@gmail.com>
2024-12-01 01:36:24 +0530

    .gitignore modified to ignore build folder

---
+++ 01-DeepCopy/build/CMakeFiles/DeepCopy.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DeepCopy.dir/deep_copy.cpp.obj"
+  "CMakeFiles/DeepCopy.dir/deep_copy.cpp.obj.d"
+  "DeepCopy.exe"
+  "DeepCopy.exe.manifest"
+  "DeepCopy.pdb"
+  "libDeepCopy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DeepCopy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 01-DeepCopy/build/CMakeFiles/DeepCopy.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DeepCopy.
+# This may be replaced when dependencies are built.
